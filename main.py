1c93e5a513107fd65ac74ec5afb5f203d739b75e
tegarimansyah <egar.imansyah@gmail.com>
2020-07-19 23:24:45 +0700

    get extension counter and file grouping

--- main.py
+++ main.py
@@ -22,11 +22,26 @@ def extract_file_data(filepath):
 def ext_mapping(list_of_files):
     return set( file_object.get('ext').lower() for file_object in list_of_files)
 
+def file_grouping(list_of_ext, list_of_files):
+    ext_counter = {}
+    ext_group = {}
+    for ext in list_of_ext:
+        list_filter = tuple(filter(lambda files: files.get('ext') == ext, list_of_files))
+
+        ext_group[ext] = list_filter
+        ext_counter[ext] = len(list_filter)
+    
+    return ext_counter, ext_group
+
 if __name__ == "__main__":
     filepaths = glob.glob(DOWNLOAD_FOLDER_PATH + '*')
     filepaths.sort(key=os.path.getmtime) # sort by time modified, unix time, asc (last is newest)
 
-    files = map(extract_file_data, filepaths)
-    extension = ext_mapping(tuple(files))
+    files = tuple(map(extract_file_data, filepaths))
+    extensions = ext_mapping(files)
+    extension_counter, file_group = file_grouping(extensions, files)
     
-    pp.pprint(extension)
+    pp.pprint(extension_counter)
+    
+    # Sample, get all .zip files 
+    pp.pprint(file_group.get('.zip'))
